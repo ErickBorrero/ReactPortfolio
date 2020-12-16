b7252e16e148074c73c67cb7d0a5d0bd25b278a1
ItsTheFlea <59777514+ItsTheFlea@users.noreply.github.com>
2020-12-16 13:09:58 -0500

    Starting over
    Working to make a more modern , responsive "all-in-one" page. Working on the navbar currently

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import NavBanner from "./components/NavBanner";
-import About from "./components/About";
-import Projects from "./components/Projects"
-import Footer from "./components/Footer"
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// import NavBanner from "./components/NavBanner";
+// import About from "./components/About";
+// import Projects from "./components/Projects"
+// import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <NavBanner />
-      <About />
-      <Projects />
-      <Footer />
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+
+export const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="">test</NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
